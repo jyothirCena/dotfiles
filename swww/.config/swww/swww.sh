27244214ba06202abc4f41ea4aad295d1bd241c7
Jyothir <191614489+jyothirCena@users.noreply.github.com>
2025-02-09 17:04:52 +0530

    added in swww

---
+++ swww/.config/swww/swww.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+wallDir=~/.config/rofi/backgrounds
+wall=$1
+
+if [ $# -eq 1 ];
+then
+
+swww img $wall
+
+rm $wallDir/*
+
+cp $wall $wallDir/currWall
+
+magick "${wall}"[0] -strip -resize 1000 -gravity center -extent 1000 -quality 90 "${wallDir}/wall.thmb"
+
+#magick "${wall}"[0] -strip -scale 10% -blur 0x3 -resize 100% "${wallDir}/wall.blur"
+
+fi
